3521fcf7efac3ef36ea6e3b3b814c90add7b15ef
vika-vlasyk <vlasyk_ak21@nuwm.edu.ua>
2023-02-13 19:49:17 +0200

    add sum_number.py

--- sum_number.py.py
+++ sum_number.py.py
@@ -18,8 +18,20 @@
 #     print(sum_number(10))
 
 
+# def sum_number(number):
+#     return sum(range(number + 1))
+
+
+# if __name__ == "__main__":
+#     print(sum_number(10))
+
+
 def sum_number(number):
-    return sum(range(number + 1))
+    try:
+        result = sum(range(number + 1))
+    except TypeError:
+        result = 0
+    return result
 
 
 if __name__ == "__main__":
