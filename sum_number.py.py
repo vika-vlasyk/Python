01331c5fc56f36e1a2591fc872c573636b038660
vika-vlasyk <vlasyk_ak21@nuwm.edu.ua>
2023-02-13 19:42:36 +0200

    add sum_number.py

---
+++ sum_number.py.py
@@ -0,0 +1,22 @@
+# def sum_number(number):
+#     num = 1
+#     result = 0
+#     while num < number + 1:
+#         result = result + num
+#         num = num + 1
+#     return result
+# if __name__ == "__main__":
+#     print(sum_number(10))
+
+
+
+
+
+def sum_number(number):
+    result = 0
+    for num in range(number + 1):
+        result += num
+    return result
+if __name__ == "__main__":
+    print(sum_number(10))
+
