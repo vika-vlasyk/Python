360d0373f643bd2b5d9041a2c892898dcc405f45
vika-vlasyk <vlasyk_ak21@nuwm.edu.ua>
2023-02-15 20:04:00 +0200

    Added implementation of more readable code

--- Game Rock scissors paper/game_project.py
+++ Game Rock scissors paper/game_project.py
@@ -1,40 +1,36 @@
 import random
 
 
-ACTIONS = ["Rock", "Paper", "Scissors"]
+ACTIONS = {0: "Rock", 1: "Paper", 2: "Scissors"}
+VICTORIES = {
+    "Rock": "Scissors",  # Rock beats scissors
+    "Paper": "Rock",  # Paper beats rock
+    "Scissors": "Paper",  # Scissors beats paper
+}
 
 
 def get_user_selection(actions):
-    choices = [f"{action}[{num}]" for num, action in enumerate(actions)]
+    choices = [f"{actions[action]}[{action}]" for action in actions]
     choices_str = ", ".join(choices)
     selection = int(input(f"Enter a choice ({choices_str}): "))
-    user_action = actions[selection]
-    return user_action
+    action = actions[selection]
+    return action
 
 
 def get_computer_selection(actions):
-    computer_action = random.choice(actions)
-    return computer_action
+    selection = random.randint(0, len(actions) - 1)
+    action = actions[selection]
+    return action
 
 
-def get_determine_winner(actions, user_action, computer_action):
+def get_determine_winner(victories, user_action, computer_action):
+    defeats = victories[user_action]
     if user_action == computer_action:
-        result = f"Both players selected {user_action}. It's a tie!\n"
-    elif user_action == actions[0]:
-        if computer_action == actions[2]:
-            result = "Rock smashes Scissors! You win!"
-        else:
-            result = "Paper covers Rock! You lose."
-    elif user_action == actions[1]:
-        if computer_action == actions[0]:
-            result = "Paper covers Rock! You win!"
-        else:
-            result = "Scissors cuts Paper! You lose."
-    elif user_action == actions[2]:
-        if computer_action == actions[1]:
-            result = "Scissors cuts Paper! You win!"
-        else:
-            result = "Rock smashes Scissors! You lose."
+        result = f"Both players selected {user_action}. It's a tie!"
+    elif computer_action in defeats:
+        result = f"{user_action} beats {computer_action}! You win!"
+    else:
+        result = f"{computer_action} beats {user_action}! You lose."
     return result
 
 
@@ -43,5 +39,7 @@ if __name__ == "__main__":
     print(user_selection)
     computer_selection = get_computer_selection(ACTIONS)
     print(computer_selection)
-    determine_winner = get_determine_winner(ACTIONS, user_selection, computer_selection)
+    determine_winner = get_determine_winner(
+        VICTORIES, user_selection, computer_selection
+    )
     print(determine_winner)
