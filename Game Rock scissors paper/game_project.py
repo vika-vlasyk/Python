03ebb595a3232685b576482e7135c8a478a9dd66
vika-vlasyk <vlasyk_ak21@nuwm.edu.ua>
2023-02-16 11:44:18 +0200

    File with comments

--- Game Rock scissors paper/game_project.py
+++ Game Rock scissors paper/game_project.py
@@ -10,6 +10,7 @@ VICTORIES = {
 
 
 def get_user_selection(actions):
+    """Функція вибору користувача"""
     choices = [f"{actions[action]}[{action}]" for action in actions]
     choices_str = ", ".join(choices)
     selection = int(input(f"Enter a choice ({choices_str}): "))
@@ -18,31 +19,33 @@ def get_user_selection(actions):
 
 
 def get_computer_selection(actions):
+    """Функція рандомного вибору комп'ютера"""
     selection = random.randint(0, len(actions) - 1)
     action = actions[selection]
     return action
 
 
 def get_determine_winner(victories, user_action, computer_action):
+    """Функція перевіряє комбінації та поакртає перемогуабо програш або нічию"""
     defeats = victories[user_action]
     if user_action == computer_action:
-        result = f"Both players selected {user_action}. It's a tie!"
+        result = f"Both players selected {user_action}. It's a tie!"  #Якщо вибір користувача співпадає з вибором комп'ютера повертається результат нічия
     elif computer_action in defeats:
-        result = f"{user_action} beats {computer_action}! You win!"
+        result = f"{user_action} beats {computer_action}! You win!"  #Якщо комбінація є парою ключь значення VICTORIES словника перемога користувача
     else:
-        result = f"{computer_action} beats {user_action}! You lose."
+        result = f"{computer_action} beats {user_action}! You lose."  #Інакше програш
     return result
 
 
 if __name__ == "__main__":
-    while True:
+    while True:  #Цикл для ого щоб гра продовжувалась
         try:
-            user_selection = get_user_selection(ACTIONS)
-            print(user_selection)
-            computer_selection = get_computer_selection(ACTIONS)
-            print(computer_selection)
+            user_selection = get_user_selection(ACTIONS)  #Вибір користувача з словника
+            print(user_selection)    #Вивід вибору користувача
+            computer_selection = get_computer_selection(ACTIONS)  #Вибір комп'ютера із словника
+            print(computer_selection)  #Вивід вибору комп'ютера
             determine_winner = get_determine_winner(
-                VICTORIES, user_selection, computer_selection
+                VICTORIES, user_selection, computer_selection  #Перевірка комбінацій визначення результату
             )
             print(determine_winner)
         except:
@@ -50,6 +53,6 @@ if __name__ == "__main__":
             print(f"Invalid selection. Enter a value in range {range_str}")
             continue
 
-        play_again = input("Play again? (y/n): ")
-        if play_again.lower() != "y":
+        play_again = input("Play again? (y/n): ")  #запит чи потрібно продовжуівати гру
+        if play_again.lower() != "y":  #якщо відповідь не "y" то гра завершується
             break
