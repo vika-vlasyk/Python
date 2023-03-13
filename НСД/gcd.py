b64be9ad91347b2fd5a68ee8324790cfe7246daa
vika-vlasyk <vlasyk_ak21@nuwm.edu.ua>
2023-03-13 18:01:37 +0200

    Додала файл з використанням pytest

--- НСД/gcd.py
+++ НСД/gcd.py
@@ -1,5 +1,11 @@
 def gcd(n, m):
-    """Compute the GCD of two integers by Euclid's algorithm."""
+    """Compute the GCD of two integers by Euclid's algorithm.
+        >>> gcd(15, 6)
+    3
+    >>> gcd(15, 5)
+    5
+    >>> gcd(-9, 15)
+    3"""
     n, m = abs(n), abs(m)
     n, m = min(n, m), max(n, m)  # Sort their absolute values.
     while m % n:  # While `n` doesn't divide into `m`:
